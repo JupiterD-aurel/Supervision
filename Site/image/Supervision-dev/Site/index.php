c5d424e818ef9e5b2912c042f0680e9cb9312525
Dhan-aurel <dboudoh@normandiewebschool.fr>
2022-09-09 02:41:18 +0200

    add image/ end part "accueil"/ end part "produit"
    commit before "abonnement"

---
+++ Site/image/Supervision-dev/Site/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/link.css">
+    <link rel="stylesheet" href="css/header.css">
+	<title>S.supervisor</title>
+</head>
+<body>
+	<nav>
+	<div class="logo">
+            <a href="index.html"><img src="image/logo.png" alt="logo-s.suppervisor" width="100px" height="50px"></div></a>
+
+  	  <div class="home">
+            <a href="#"><strong>PRODUIT</strong></a>
+            <a href="#"><strong>HISTOIRE</strong></a>
+            <a href="#"><strong>CONTACT</strong></a>
+            <a href="#" id="lgnbutton"><strong>SE CONNECTER</strong></a>  
+                
+        </div>
+
+	</nav>
+
+	<div class="barre"></div>
+
+	<div class="lesH">
+		<h1>STATION SUPERVISOR</h1>
+		<h2>TRANSPARENCE</h2>
+		<h2>LOYAUTÉ</h2>
+		<h2>RESPECT</h2>
+		<h2>PROXIMIÉ</h2>
+	</div>
+
+		<div class="text-box">
+			<a href="#" class="btn btn-white btn-animate">En savoir plus</a>
+			<a href="#" class="btn btn-white btn-animate">Contact</a>
+		</div>
+
+<div class="produit">
+	<div class="rectangle1"></div>
+	<div class="rectangle2">
+	<div class="text1">
+		<h1>Produit.</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a laoreet est. Fusce eleifend mattis urna. Suspendisse potenti. Sed vestibulum ligula vel metus semper, nec euismod sem aliquam. Maecenas bibendum odio leo, nec mattis ipsum accumsan sit amet. Suspendisse metus ipsum, egestas at varius</p>
+	</div></div>
+</div>
+
+<div class="abonnement">
+		<h1>Abonnement.</h1>
+
+	<div class="formules">
+
+		<div class="standard">
+			<h1>Standard</h1>
+				<h1>2€*</h1>
+		<div>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+		</div>
+		<div class="text-box">
+			<a href="#" class="btn btn-white btn-animate">S'abonner</a>
+		</div>
+	</div>
+
+		<div class="premium">
+
+			<h1>Premium</h1>
+				<h1>6€*</h1>
+
+			<div>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+			</div>
+		<div class="text-box">
+			<a href="#" class="btn btn-white btn-animate">S'abonner</a>
+		</div>
+</div>
+	</div>
+
+	<div class="valeurs">
+		<h2>90 000</h2> 
+			<h3> points de recharge financés sur un objectif de 125 000 d’ici 2025</h3>
+		<h2>320</h2> 
+			<h3>millions d’euros de budget </h3>
+		<h2>2025</h2> 
+			<h3>fin du programme</h3>
+	</div>
+
+</div>
+
+<div class="barre1"></div>
+
+	<div class="histoire">
+		<div class="part1">
+			<h1>Histoire.</h1>
+			<h2>Hi, ik ben Jasper</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a laoreet est. Fusce eleifend mattis urna. Suspendisse potenti. Sed vestibulum ligula vel metus semper, nec euismod sem aliquam. Maecenas bibendum odio leo, nec mattis ipsum accumsan sit amet. Suspendisse metus ipsum, egestas at varius</p>
+		</div>
+
+		<div class="part2">
+
+		</div>
+	</div>
+
+	
+
+</body>
+</html>
--- Site/index.php
+++ Site/index.php
@@ -31,7 +31,11 @@
 				<img src="image/vivimobile.svg" alt="mascotte vivi">
 			</div>
 		</div>
-
+		<div class="icon">            
+			<a href="#"><img src="image/icons8-twitter.gif" alt="icon-twitter" width="50px" height="50px"></a>
+            <a href="#"><img src="image/icons8-linkedin-2.gif" alt="icon-LinkedIn" width="50px" height="50px"></a>
+            <a href="#"><img src="image/icons8-instagram.gif" alt="Instagrame" width="50px" height="50px"></a>
+		</div>
 	</div>
 		<div class="accueil-bottom">
 
@@ -42,6 +46,78 @@
 
 		</div>
 
+<div id="intern">
+
+<div class="produit">
+	<div class="rectangle1"><img src="image/charging-pistol-in-socket-at-charging-station.jpg" alt="charging"></div>
+	<div class="rectangle2">
+	<div class="text1">
+		<h1>Produit.</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a laoreet est. Fusce eleifend mattis urna. Suspendisse potenti. Sed vestibulum ligula vel metus semper, nec euismod sem aliquam. Maecenas bibendum odio leo, nec mattis ipsum accumsan sit amet. Suspendisse metus ipsum, egestas at varius</p>
+	</div>
+
+			<div class="text-box">
+				<a href="#" class="btn btn-white btn-animate">En savoir plus</a>
+				<a href="#" class="btn btn-white btn-animate">NOUS CONTACTER</a>
+			</div>
+
+</div>
+
+
+</div>
+
+<div class="abonnement">
+		<h1>Abonnement.</h1>
+
+	<div class="formules">
+
+		<div class="standard">
+			<h1>Standard</h1>
+				<h1>2€*</h1>
+		<div>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+		</div>
+		<div class="text-box">
+			<a href="#" class="btn btn-white btn-animate">S'abonner</a>
+		</div>
+	</div>
+
+		<div class="premium">
+
+			<h1>Premium</h1>
+				<h1>6€*</h1>
+
+			<div>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+				<p>✔️ Lorem Ipsum</p>
+			</div>
+		<div class="text-box">
+			<a href="#" class="btn btn-white btn-animate">S'abonner</a>
+		</div>
+</div>
+	</div>
+
+	<div class="valeurs">
+		<h2>90 000</h2> 
+			<h3> points de recharge financés sur un objectif de 125 000 d’ici 2025</h3>
+		<h2>320</h2> 
+			<h3>millions d’euros de budget </h3>
+		<h2>2025</h2> 
+			<h3>fin du programme</h3>
+	</div>
+
+</div>
+
+</div>
 
 
 </body>
