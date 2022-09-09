519cdf5284d22c04cd539d232676e925e82eb342
Dhan-aurel <dboudoh@normandiewebschool.fr>
2022-09-09 13:14:38 +0200

    pull final

--- Site/index.php
+++ Site/index.php
@@ -70,17 +70,15 @@
 		<h1>Abonnement.</h1>
 
 	<div class="formules">
-
 		<div class="standard">
 			<h3>Standard</h3>
 				<h4>2€/mois HT</h4>
 		<div>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>❌ Lorem Ipsum</p>
-				<p>❌ Lorem Ipsum</p>
+				<p>✔️ Une page d’historique des transactions</p>
+				<p>✔️ Une page de gestion utilisateur</p>
+				<p>✔️ Un conseiller disponible de 8h30 à 17h30 par téléphone.</p>
+				<p>❌ Un système de détecteur de “véhicule ventouse” (par exemple les véhicules qui reste trop longtemps sur une borne de recharge électrique,<br> si un véhicule est déjà complètement chargé)</p>
+				<p>❌ La possibilité de faire des réconciliations avec le compte bancaire des clients.</p>
 		</div>
 
 		<div class="stampede">
@@ -93,13 +91,15 @@
 			<h3>Premium</h3>
 				<h4>6€/mois HT</h4>
 
-			<div>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
+			<div id="pre">
+				<p id="lignepetite1">✔️ Une page d’historique des transactions</p>
+				<p id="lignepetite2">✔️ Une page de gestion utilisateur</p>
+				<p id="lignepetite3">✔️ Un conseiller disponible de 8h30 à 17h30 par téléphone.</p>
+				<p id="lignepetite4">✔️ Un système de détecteur de “véhicule ventouse” (par exemple les véhicules qui reste trop longtemps sur une borne de recharge électrique,<br> si un véhicule est déjà complètement chargé)</p>
+				<p id="lignepetite5">✔️ La possibilité de faire des réconciliations avec le compte bancaire des clients.</p>
+				<p id="lignepetite6">✔️ Un système de ticketing pour une meilleure traçabilité des échanges 24h/24h, très utile pour les grosses structures.</p>
+				<p id="lignepetite7">✔️ Un conseiller disponible 24h/24h pour répondre à toutes vos questions.</p>
+				<p id="lignepetite">✔️ La fonctionnalité OCPP (Open Charge Point Protocol) qui permet un redémarrage à distance.</p>
 			</div>
 
 		<div class="stampede">
--- Site/service.php
+++ Site/service.php
@@ -23,25 +23,13 @@
 					<a href="contact.php" class="btn btn-white btn-animate">NOUS CONTACTER</a>
 			</div></div>
 </div>
-		<div class="text"></div>
+		<div class="tex">
 
 					<h1>Service.</h1>
-		<h2> Nos offres pour la supervision des bornes de recharge électrique </h2>
 
-		Nous vous proposons 2 forfaits de logiciel de supervision de bornes :
+		<h3> Nos offres pour la supervision des bornes de recharge électrique </h3>
 
-		Forfait 2 €/mois (hors taxe), qui comprend:
-		- Une page d’historique des transactions
-		- Une page de gestion utilisateur
-		- Un conseiller disponible de 8h30 à 17h30 par téléphone.
-
-
-		Forfait 6 €/mois (hors taxe), qui comprend en plus les avantages suivants:
-		- Un système de détecteur de “véhicule ventouse” (par exemple les véhicules qui reste trop longtemps sur une borne de recharge électrique, si un véhicule est déjà complètement chargé)
-		- La possibilité de faire des réconciliations avec le compte bancaire des clients.
-		- Un système de ticketing pour une meilleure traçabilité des échanges 24h/24h, très utile pour les grosses structures.
-		- Un conseiller disponible 24h/24h pour répondre à toutes vos questions.
-		- La fonctionnalité OCPP (Open Charge Point Protocol) qui permet un redémarrage à distance. 
+		<h3>Nous vous proposons 2 forfaits de logiciel de supervision de bornes : </h3>
 
 		</div>
 
@@ -50,18 +38,29 @@
 <div class="abonnement"> <a name="abon"></a>
 		<h1>Abonnement.</h1>
 
+<div class="forfait">
+
+		<div id="euro">
+				<h5>Forfait 2 €/mois (hors taxe), qui comprend:</h5>
+		</div>
+
+		<div id="euro">
+				<h5>Forfait 6 €/mois (hors taxe), qui comprend en plus les avantages suivants:</h5>
+		</div>
+</div>
+
+
 	<div class="formules">
 
 		<div class="standard">
 			<h3>Standard</h3>
 				<h4>2€/mois HT</h4>
 		<div>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>❌ Lorem Ipsum</p>
-				<p>❌ Lorem Ipsum</p>
+				<p>✔️ Une page d’historique des transactions</p>
+				<p>✔️ Une page de gestion utilisateur</p>
+				<p>✔️ Un conseiller disponible de 8h30 à 17h30 par téléphone.</p>
+				<p>❌ Un système de détecteur de “véhicule ventouse” (par exemple les véhicules qui reste trop longtemps sur une borne de recharge électrique,<br> si un véhicule est déjà complètement chargé)</p>
+				<p>❌ La possibilité de faire des réconciliations avec le compte bancaire des clients.</p>
 		</div>
 
 		<div class="stampede">
@@ -74,13 +73,15 @@
 			<h3>Premium</h3>
 				<h4>6€/mois HT</h4>
 
-			<div>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
-				<p>✔️ Lorem Ipsum</p>
+			<div id="pre">
+				<p id="lignepetite1">✔️ Une page d’historique des transactions</p>
+				<p id="lignepetite2">✔️ Une page de gestion utilisateur</p>
+				<p id="lignepetite3">✔️ Un conseiller disponible de 8h30 à 17h30 par téléphone.</p>
+				<p id="lignepetite4">✔️ Un système de détecteur de “véhicule ventouse” (par exemple les véhicules qui reste trop longtemps sur une borne de recharge électrique,<br> si un véhicule est déjà complètement chargé)</p>
+				<p id="lignepetite5">✔️ La possibilité de faire des réconciliations avec le compte bancaire des clients.</p>
+				<p id="lignepetite6">✔️ Un système de ticketing pour une meilleure traçabilité des échanges 24h/24h, très utile pour les grosses structures.</p>
+				<p id="lignepetite7">✔️ Un conseiller disponible 24h/24h pour répondre à toutes vos questions.</p>
+				<p id="lignepetite">✔️ La fonctionnalité OCPP (Open Charge Point Protocol) qui permet un redémarrage à distance.</p>
 			</div>
 
 		<div class="stampede">
